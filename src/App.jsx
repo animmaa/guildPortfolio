63ed9f2eb66901a874fbfdb5fd0861d4c36f2ea5
davy lebris <davylebris@gmail.com>
2024-10-05 13:03:59 +0200

    add scss, wallpeper image guild

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import fondEcran from './assets/Guilde.png';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={fondEcran} className="Wallpaper" alt="wallpaper" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
